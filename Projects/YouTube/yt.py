9c87ddd8549fadc1c7b28866a96ae1180aeac543
Prince Kumar <pk861510@gmail.com>
2025-12-12 11:38:12 +0530

    implement script to calculate total YouTube playlist duration

--- Projects/YouTube/yt.py
+++ Projects/YouTube/yt.py
@@ -5,7 +5,11 @@ load_dotenv()
 
 api_key = os.getenv('api_key')
 youtube = build('youtube', 'v3',developerKey = api_key)
-request = youtube.channels().list(forUsername = 'schafer5', part =  'statistics')
+request = youtube.channels().list(
+forUsername = 'schafer5'
+, part =  'contentDetails,statistics')
+
+
 response = request.execute()
 print(response)
 youtube.close()
